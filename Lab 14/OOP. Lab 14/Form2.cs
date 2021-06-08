ff071940e19a86165ee2208ff1aa13c38f28143e
GeorGius11 <79843910+GeorGius11@users.noreply.github.com>
2021-06-09 00:44:26 +0300

    Lab14

---
+++ Lab 14/OOP. Lab 14/Form2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP.Lab_14
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (BinaryReader br = new BinaryReader(File.Open(Form1.path, FileMode.Open)))
+                {
+                    while (br.BaseStream.Position < br.BaseStream.Length)
+                    {
+                        dataGridView1.Rows.Add(
+                            br.ReadInt32(),
+                            br.ReadString(),
+                            br.ReadString(),
+                            br.ReadInt32(),
+                            br.ReadInt32(),
+                            br.ReadString(),
+                            br.ReadBoolean());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+                return;
+            }
+
+        }
+    }
+}
